e7855f76ff47120480d3db14f8165489cb6482ea
Jason Graziano <mrehawk82@gmail.com>
2019-05-20 19:50:26 -0400

    ...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,18 +4,14 @@ var items = [ "Assess Risks", "Identify Hazards", "Make Risk Decisions", "Implem
 
 myUI = {
 	init: () => {
-		console.log("init");
-        
 		myUI.myLoad();
 	},
 	toggleFullScreen: () => {
         return () => {
 			if (!document.fullscreenElement) {
-
             	document.documentElement.requestFullscreen();
         	} else {
             	if (document.exitFullscreen) {
-     
                 	document.exitFullscreen();
                 }
             }
@@ -24,72 +20,115 @@ myUI = {
 	myLoad: () => {
         var tCell = createEle("div"),
             b1 = createEle("button"),
-            arrow2Btn = createEle("button"),
-            arrow1Btn = createEle("button"),
             climateBtn = createEle("button"),
             goBtn = createEle("button"),
             homeBtn = createEle("button"),
-            statsBtn = createEle("button");
+            statsBtn = createEle("button"),
+            climatePage = createEle("div"),
+            goPage = createEle("div"),
+            statsPage = createEle("div"),
+            homePage = createEle("div");
         
-        arrow2Btn.innerHTML = "🔻";
-        arrow2Btn.className = "arrow2Btn";
-        arrow2Btn.onclick = myUI.arrow2Func(arrow1Btn, arrow2Btn, tCell);
+        homePage.innerHTML = "<h1>COMMAND POST</h1>";
+        homePage.className = "pages";
+        homePage.id = "homePage";
 
-        arrow1Btn.innerHTML = "🔺";
-        arrow1Btn.onclick = myUI.arrow1Func(arrow1Btn, arrow2Btn, tCell);
+        statsPage.innerHTML = "<h1>STAT LOG</h1>";
+        statsPage.className = "pages";
+        statsPage.id = "statsPage";
+
+        climatePage.innerHTML = "<h1>CLIMATE RECON</h1>";
+        climatePage.className = "pages";
+        climatePage.id = "climatePage";
+
+        goPage.innerHTML = "<h1>CONTINGENCY REPORT</h1>";
+        goPage.className = "pages";
+        goPage.id = "goPage";
 
         climateBtn.innerHTML = "⛅";
-        climateBtn.onclick = myUI.climateScreen();
+        climateBtn.onclick = myUI.climateScreen(climateBtn, homePage, statsPage, climatePage, goPage);
+        climateBtn.id = "climateBtn";
+        climateBtn.disabled = false;
 
         goBtn.innerHTML = "🚦";
-        goBtn.onclick = myUI.goScreen();
+        goBtn.onclick = myUI.goScreen(goBtn, homePage, statsPage, climatePage, goPage);
+        goBtn.id = "goBtn";
+        goBtn.disabled = false;
 
         statsBtn.innerHTML = "〽";
-        statsBtn.onclick = myUI.statsScreen();
+        statsBtn.onclick = myUI.statsScreen(statsBtn, homePage, statsPage, climatePage, goPage);
+        statsBtn.id = "statsBtn";
+        statsBtn.disabled = false;
 
         homeBtn.innerHTML = "&#127969;";
-        homeBtn.onclick = myUI.homeScreen();
+        homeBtn.onclick = myUI.homeScreen(homeBtn, homePage, statsPage, climatePage, goPage);
+        homeBtn.id = "homeBtn";
+        homeBtn.disabled = true;
 
         b1.innerHTML = "🔳";
         b1.onclick = myUI.toggleFullScreen();
 
         tCell.className = "tCell";
-        tCell.append(b1, homeBtn, statsBtn, goBtn, climateBtn, arrow1Btn);
+        tCell.append(b1, homeBtn, statsBtn, goBtn, climateBtn);
 
-		body.append(tCell, arrow2Btn);
+		body.append(tCell, homePage, statsPage, climatePage, goPage);
 
-        setTimeout(function(){ makeFull( tCell); }, 150);
+        setTimeout(function(){ makeFull( tCell);makeFull(homePage); }, 150);
 	},
-    homeScreen: function(){
+    homeScreen: function(home, homePage, statsPage, climatePage, goPage){
         return function(){
-            alert("homeScreen function");
+            var x = bySelAll(".tCell_full button");
+            for (var i = 0; i < x.length; i++){
+                if (x[i].disabled === true) {
+                    x[i].disabled = false;
+                    var y = x[i].id.split("Btn"), z = bySel("#" + y[0] + "Page");
+                    takeFull(z);
+                    homeBtn.disabled = true;
+                    makeFull(homePage);
+                }
+            }
         }
     },
-    statsScreen: function(){
+    statsScreen: function(statsBtn, homePage, statsPage, climatePage, goPage){
         return function(){
-            alert("statsScreen function");
+            var x = bySelAll(".tCell_full button");
+            for (var i = 0; i < x.length; i++){
+                if (x[i].disabled === true) {
+                    x[i].disabled = false;
+                    var y = x[i].id.split("Btn"), z = bySel("#" + y[0] + "Page");
+                    takeFull(z);
+                    statsBtn.disabled = true;
+                    makeFull(statsPage);
+                }
+            }
         }
     },
-    goScreen: function(){
+    goScreen: function(goBtn, homePage, statsPage, climatePage, goPage){
         return function(){
-            alert("goScreen function");
+            var x = bySelAll(".tCell_full button");
+            for (var i = 0; i < x.length; i++){
+                if (x[i].disabled === true) {
+                    x[i].disabled = false;
+                    var y = x[i].id.split("Btn"), z = bySel("#" + y[0] + "Page");
+                    takeFull(z);
+                    goBtn.disabled = true;
+                    makeFull(goPage);
+                }
+            }
         }
     },
-    climateScreen: function(){
+    climateScreen: function(climateBtn, homePage, statsPage, climatePage, goPage){
         return function(){
-            alert("climateScreen function");
-        }
-    },
-    arrow1Func: function(arrow1Btn, arrow2Btn, tCell){
-        return function(){
-            makeFull(arrow2Btn);
-            takeFull(tCell);
-        }
-    },
-    arrow2Func: function(arrow1Btn, arrow2Btn, tCell){
-        return function(){
-            makeFull(tCell);
-            takeFull(arrow2Btn);
+            var x = bySelAll(".tCell_full button");
+            for (var i = 0; i < x.length; i++){
+                if (x[i].disabled === true) {
+                    x[i].disabled = false;
+                    var y = x[i].id.split("Btn"), z = bySel("#" + y[0] + "Page");
+                    takeFull(z);
+                    climateBtn.disabled = true;
+                    makeFull(climatePage);
+                }
+            }
         }
     }
 };
